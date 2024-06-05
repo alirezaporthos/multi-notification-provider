cb4db1322fc464341b0beee2fe20a854417434c6
alirezaporthos <alirezaportous@gmail.com>
2024-06-05 14:10:05 +0330

    Add Plivo as a sms provider

---
+++ app/Services/PorthosNotification/Providers/Sms/PlivoSmsServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services\PorthosNotification\Providers\Sms;
+
+use App\Services\PorthosNotification\Contracts\Sms\SmsProviderInterface;
+use Illuminate\Support\Facades\Log;
+use Plivo\Exceptions\PlivoRestException;
+use Plivo\RestClient;
+
+class PlivoSmsServiceProvider implements SmsProviderInterface
+{
+    private $plivoClient;
+
+    public function __construct()
+    {
+        $this->plivoClient = new RestClient(
+            env('PLIVO_AUTH_TOKEN'),
+            env('PLIVO_PHONE_NUMBER')
+        );
+    }
+
+
+    public function send($message, $recipient): bool
+    {
+        try {
+            $response = $this->plivoClient->messages->create(
+                $recipient,
+                [
+                    'text' => $message,
+                    'from' => env('PLIVO_PHONE_NUMBER'),
+                ]
+            );
+        } catch (PlivoRestException $exception) {
+            Log::error("Failed to send SMS via Plivo: {$exception->getMessage()}");
+            return false;
+        }
+        return true;
+    }
+}
