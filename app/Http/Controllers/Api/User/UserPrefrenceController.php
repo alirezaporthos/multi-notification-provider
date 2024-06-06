6b40d28468b5fc24f3d03d8a80a7d63d0d2cbde5
alirezaporthos <alirezaportous@gmail.com>
2024-06-07 00:05:50 +0330

    Add user prefrence update mechanism

--- app/Http/Controllers/Api/User/UserPrefrenceController.php
+++ app/Http/Controllers/Api/User/UserPrefrenceController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\User\StoreUserPrefrenceRequest;
+use App\Http\Requests\Api\User\UpdateUserPrefrenceRequest;
 use App\Http\Resources\UserPrefrenceResource;
 use App\Models\User;
+use App\Models\UserPrefrence;
 use Illuminate\Http\JsonResponse;
 
 class UserPrefrenceController extends Controller
@@ -19,6 +21,18 @@ class UserPrefrenceController extends Controller
         return response()->json(
             new UserPrefrenceResource($userPrefrence),
             201
+        );
+    }
+
+    public function update(User $user, UserPrefrence $userPrefrence, UpdateUserPrefrenceRequest $request)
+    {
+        $userPrefrence->update([
+            'notification_prefrences' => $request->notification_prefrences
+        ]);
+
+        return response()->json(
+            new UserPrefrenceResource($userPrefrence),
+            200
         );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/users/{user}')->name('users.*')->group(function () {
     Route::prefix('user-prefrences')->name('user-prefrences.*')->group(function () {
         Route::post('/', [UserPrefrenceController::class, 'store'])->name('store');
+        Route::put('/{userPrefrence}', [UserPrefrenceController::class, 'update'])->name('update');
     });
 });
