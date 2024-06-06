0d07cf442d5c0ca60e3081494b29170bc2840482
alirezaporthos <alirezaportous@gmail.com>
2024-06-06 23:39:38 +0330

    Add factory for user prefrence

---
+++ database/factories/UserPrefrenceFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserPrefrence>
+ */
+class UserPrefrenceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'notification_prefrences' => $this->faker->randomElement(['sms', 'email']),
+        ];
+    }
+}
